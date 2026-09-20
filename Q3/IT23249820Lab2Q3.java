74e3088b4a8d01ad169e598d4e8341d4c3337ac3
Hiruni Sanjala <hirunisanjala4@gmail.com>
2026-09-20 21:47:07 +0530

    Add Lab 2 Question 3 answer

---
+++ Q3/IT23249820Lab2Q3.java
@@ -0,0 +1,13 @@
+public class IT23249820Lab2Q3 {
+
+    public static void main(String[] args) {
+
+        double sideA = 3.0;
+        double sideB = 4.0;
+
+        double hypotenuse =
+                Math.sqrt((sideA * sideA) + (sideB * sideB));
+
+        System.out.println("Length of the hypotenuse: " + hypotenuse);
+    }
+}
